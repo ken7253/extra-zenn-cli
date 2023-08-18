04325a94f343056486628fce4579598fb61b5b93
ken7253 <ken7253@gmail.com>
2023-08-18 14:55:45 +0900

    listコマンドの基本形を実装

--- src/commands/list/index.ts
+++ src/commands/list/index.ts
@@ -1,12 +1,31 @@
+import path from 'node:path';
+
 import { safeParse } from 'valibot';
+import { glob } from 'glob';
 
 import { argNormalizer } from '../../util/argNormalizer';
 import { ArgSchema } from './schema';
 
+/**
+ * list command
+ * @param args `cac`によって処理される引数のオブジェクト
+ */
 export const list = (args: unknown): void => {
-	const validArgs = safeParse(ArgSchema, argNormalizer(args));
+	const validResult = safeParse(ArgSchema, argNormalizer(args));
 
-	if (validArgs.success) {
-		console.log(validArgs.data);
+	if (!validResult.success) {
+		console.log('augment parse error');
+		throw validResult.error;
 	}
+
+	const option = validResult.data;
+	const baseDir = path.join(process.cwd(), option.books ? 'books' : 'articles');
+	void glob(path.join(baseDir, '*.md')).then((filePathList) => {
+		const fileList = filePathList.reduce<string[]>((acc, current) => {
+			const { name, ext } = path.parse(current);
+			return ext !== '.md' ? [...acc] : [...acc, `${name}${ext}`];
+		}, []);
+
+		console.log(fileList.join(' '));
+	});
 };
