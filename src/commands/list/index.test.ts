82172c9ffb5d47f00f94885bef6ce1b9bfee0f90
ken7253 <ken7253@gmail.com>
2023-08-18 20:18:15 +0900

    テストケースを追加

--- src/commands/list/index.test.ts
+++ src/commands/list/index.test.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { describe, test, expect } from 'vitest';
 
 import { list } from './index';
@@ -6,7 +7,18 @@ import type { ArgData } from './schema';
 describe('各種ファイルの一覧を出力するためのコマンド', () => {
 	describe('オプションを指定しない場合', () => {
 		test('コマンドが実行されること', () => {
+			// Assert
 			expect(() => list<ArgData>({ books: false })).not.toThrowError();
+		});
+
+		test('Markdownファイルのみ一覧で取得できること', async () => {
+			// Act
+			const stdout = await list<ArgData>({ books: false });
+			const fileNameList = stdout.split(' ');
+			const extNameList = fileNameList.map((name) => path.parse(name).ext);
+			const nonMarkdownFile = extNameList.filter((v) => v !== '.md');
+			// Assert
+			expect(nonMarkdownFile).toStrictEqual([]);
 		});
 	});
 });
