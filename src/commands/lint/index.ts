0d55726680d7ea93aa9fefff2f247c8d38ad51e1
ken7253 <ken7253@gmail.com>
2024-07-18 01:08:12 +0900

    とりあえずエラーログを出力できるように

--- src/commands/lint/index.ts
+++ src/commands/lint/index.ts
@@ -10,19 +10,38 @@ import type { Command } from '../types';
 export const lint: Command = async () => {
 	const baseDir = path.join(process.cwd(), 'articles');
 	const fileList = await glob(path.join(baseDir, '*.md'));
-	const fileData = fileList.map((filePath) =>
-		readFile(filePath, { encoding: 'utf-8' }),
-	);
-	const frontMatter = (await Promise.all(fileData)).map((file) => fm(file));
 
-	const attributesList = frontMatter
-		.filter((v) => Object.hasOwn(v, 'attributes'))
-		.map((v) => v.attributes);
-	const r = attributesList.map((attr) => {
-		if (!(attr instanceof Object)) return;
-		return validateArticle(attr);
+	const validate = fileList.map(async (path) => {
+		const file = await readFile(path, { encoding: 'utf-8' });
+		const { attributes } = fm(file);
+		if (attributes == null) return [path, null] as const;
+		return [path, validateArticle(attributes)] as const;
+	});
+
+	const result = await Promise.all(validate);
+
+	const format = result.flatMap(([path, detail]) => {
+		if (detail === null) return [''];
+
+		const error = detail.filter(({ isCritical }) => isCritical);
+		const warn = detail.filter(({ isCritical }) => !isCritical);
+
+		const errorList = error
+			.map(({ type, message }) => [type, message].join('\n'))
+			.join('\n');
+		const warnList = error
+			.map(({ type, message }) => [type, message].join('\n'))
+			.join('\n');
+
+		return [
+			`${path}`,
+			`Error: ${error.length}`,
+			`${errorList}`,
+			`Warn: ${warn.length}`,
+			`${warnList}`,
+		].join('\n');
 	});
 
 	// Lint
-	return JSON.stringify(r, undefined, 2);
+	return format.join('\n');
 };
